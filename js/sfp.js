9b23db1cad7e0b68ee650b78325d39b0b895b676
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2016-06-08 19:48:52 +0200

    Included tether.

--- js/sfp.js
+++ js/sfp.js
@@ -6,3 +6,25 @@ function alertAndClear(formID){
 function resetForm(formID){
             document.getElementById(formID).reset();
         }
+function submitForm(){
+    var form = document.getElementById("contactForm");
+            var replyto = form.elements[0].value;
+              var subject = form.elements[1].value;
+               var msg = form.elements[2].value;
+               var gotcha =  form.elements[3].value;
+            $.ajax({
+                url: "https://formspree.io/{{site.email}}",
+                method: "POST",
+                data: {
+                    _replyto: replyto,
+                    _subject: subject,
+                    message: msg,
+                    _gotcha: gotcha
+                },
+                dataType: "json"
+            });
+            $("#formDiv").hide();
+            $("#footerLogo").hide();
+            $("#thankYou").show();
+            return false;
+}
