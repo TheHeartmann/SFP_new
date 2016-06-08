6b44602a95638598507e7fc190a79c631fafb05b
Thomas Hartmann <thomas.o.hartmann@gmail.com>
2016-06-08 19:44:59 +0200

    Almost there.

--- js/sfp.js
+++ js/sfp.js
@@ -2,3 +2,7 @@ function alertAndClear(formID){
     alert('Thank you for getting in touch. We will process your enquiry and get back to you shortly.');
     document.getElementById(formID).reset();
 }
+
+function resetForm(formID){
+            document.getElementById(formID).reset();
+        }
